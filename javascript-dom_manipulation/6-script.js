b20f5e0c5a2072421b0fc81fe3ef1638c9fad0b3
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-14 23:27:11 -0500

    This JavaScript code gets the name of a character from a URL using the Fetch API and displays it in the HTML element with the ID 'character'. In case of a network error, handles the error by displaying an appropriate message

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,29 @@
+// Wait for the DOM to be fully loaded before attaching event listeners
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the element with id "character"
+    const characterElement = document.getElementById('character');
+  
+    // URL to fetch the character's name
+    const apiUrl = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  
+    // Make a Fetch request to the API
+    fetch(apiUrl)
+      .then(response => {
+        // Check if the request was successful
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the JSON response
+      })
+      .then(data => {
+        // Get the character's name from the data
+        const characterName = data.name;
+  
+        // Display the character's name in the "character" element
+        characterElement.textContent = characterName;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        characterElement.textContent = 'Error fetching data';
+      });
+});
