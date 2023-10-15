ff5af1d3a76c66a16b3817314c5ea3afae3f0963
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-14 23:01:22 -0500

    Script to add a li element to a list when the user clicks on the element with id add_item. The new element must be: <li>Item</li>. The new element will be added to the ul element with class my_list

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the element with id "add_item"
+    const addItemButton = document.getElementById('add_item');
+  
+    // Select the "ul" element with class "my_list"
+    const myList = document.querySelector('.my_list');
+  
+    // Add a click event listener to the "add_item" element
+    addItemButton.addEventListener('click', function () {
+      // Create a new "li" element
+      const newItem = document.createElement('li');
+      newItem.textContent = 'Item'; // Set the text content of the new "li" element
+  
+      // Append the new "li" element to the "my_list" ul
+      myList.appendChild(newItem);
+    });
+});  
