118f01eb5f9e984c873573cf7809824ea7163ecf
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-19 20:05:40 -0500

    JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header with a JQuery API

--- javascript-web_jquery/1-script.js
+++ javascript-web_jquery/1-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     // Use jQuery to select the <header> element and change its text color to red
     $('header').css('color', '#FF0000');
-  });
+});
   
--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     // Attach a click event handler to the #red_header element
     $('#red_header').click(function() {
-      // Use jQuery to select the <header> element and change its text color to red
-      $('header').css('color', '#FF0000');
+      // Use jQuery to select the <header> element and add the "red" class
+      $('header').addClass('red');
     });
 });
   
