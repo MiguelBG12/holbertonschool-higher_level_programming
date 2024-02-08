78a38fa73306aabe9d5793577129ef196873339d
Miguel Bautista <135651530+MiguelBG12@users.noreply.github.com>
2024-02-08 10:26:56 -0500

    Delete javascript-dom_manipulation directory

--- javascript-dom_manipulation/0-script.js
+++
@@ -1,5 +0,0 @@
-// Select the header element by its tag name
-const etiquetaheader = document.querySelector("header");
-
-// Update the text color to red (#FF0000)
-etiquetaheader.style.color = "#FF0000";
