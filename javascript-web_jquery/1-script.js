69613a39223a16a3ddb799a0208ffaae2e6c4ba2
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-15 14:46:55 -0500

    Using jQuery, this code waits for the document to be fully loaded and then changes the text color of the header element to red.

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Wait for the document to be fully loaded
+$(document).ready(() => {
+  // Select the header element and change its text color to red using jQuery
+  $('header').css('color', 'red');
+});
