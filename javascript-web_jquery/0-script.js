90c1318487d739583309c0796830dc3f65ccd51f
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-15 14:33:12 -0500

    Selects the header element in the document and changes its text color to red.

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Select the header element from the document
+const element = document.querySelector("header");
+
+// Change the color style of the header element to red
+element.style.color = "#FF0000";
